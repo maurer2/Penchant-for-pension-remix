6a281a7c401a4448e56c4845afe6f0884c432770
Martin <mr.mmaurer@gmail.com>
2024-02-06 23:49:56 +0000

    Added params validation via route loader

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
-import { useSearchParams } from "@remix-run/react";
-
+import { useSearchParams, useLoaderData } from "@remix-run/react";
+import { z } from "zod";
+import { json } from "@remix-run/node"; // or cloudflare/deno
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,14 +10,44 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+type RootLoaderProps = {
+  request: Request;
+};
+
+const queryParamsSchema = z.object({
+  desiredPension: z.string().catch("1"),
+  personalContribution: z.string().catch("2"),
+  employerContribution: z.string().catch("3"),
+  retirementAge: z.string().catch("4"),
+});
+
+type QueryParamsSchema = z.infer<typeof queryParamsSchema>;
+
+export async function loader({ request }: RootLoaderProps) {
+  const queryParamsStringified = Object.fromEntries(
+    new URL(request.url).searchParams
+  );
+
+  try {
+    const queryParams: QueryParamsSchema = queryParamsSchema.parse(
+      queryParamsStringified
+    );
+    return json(queryParams);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 // ?desiredPension=1&personalContribution=2&employerContribution=3&retirementAge=4
 export default function Index() {
-  const [searchParams, setSearchParams] = useSearchParams();
+  // const [searchParams, setSearchParams] = useSearchParams();
 
-  const desiredPension = searchParams.get('desiredPension');
-  const personalContribution = searchParams.get('personalContribution');
-  const employerContribution = searchParams.get('employerContribution');
-  const retirementAge = searchParams.get('retirementAge');
+  const {
+    desiredPension,
+    personalContribution,
+    employerContribution,
+    retirementAge,
+  } = useLoaderData<typeof loader>();
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif" }}>
